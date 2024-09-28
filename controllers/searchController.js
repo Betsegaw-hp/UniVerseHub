fef2a870c0256bd63c0fe8714cd6d8207bfad145
betsegaw-hp <betse53@gmail.com>
2024-09-28 13:23:35 +0300

    changed bootstrap cdn to local

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -3,6 +3,7 @@ const { ForumPost } = require('../model/forum');
 const search_topic = async (req, res) => {
    const topicTitle = req.params.title ;
 
+   return res.status(500).json({ msg: "not implemented yet!" });
    try {
     
     const posts = await ForumPost.find();
